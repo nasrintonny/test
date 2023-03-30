ced1e07e7fb1c3e4aba6d73da7f484e012c184d4
nasrintonny <129414726+nasrintonny@users.noreply.github.com>
2023-03-30 23:29:02 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+
+
+
+<script>
+    function multiplicationTable(num) {
+        for (let i = 1; i <= 10; i++) {
+            document.write(`${num} * ${i} = ${num * i} <br/>`);
+        }
+    }
+    multiplicationTable(10);
+</script>
